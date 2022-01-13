d2d8d795f191a12d6ae08ea81399ea7cf37a413d
xunnhwa <xunnhwa1999@gmail.com>
2022-01-13 13:50:19 +0900

    allowed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xdrh*d81%kjhqnd7yt%v7$7g-w0oi@c-g(44%0w_5qx#(n(4vp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.169.111']
 
 
 # Application definition
